f8b40ab3cf2951e68093c718f5e24f6be309ecfb
lhourquin <lhourquin@gmail.com>
2022-02-06 07:58:02 +0100

    style sheet today times list li with span for information, and position of container ul for mobile device

--- src/components/today-components/today-time-list/TodayTimesList.js
+++ src/components/today-components/today-time-list/TodayTimesList.js
@@ -1,40 +1,53 @@
 import React from "react";
 import "./TodayTimesList.css";
-export const TodayTimesList = ({today}) => {
+export const TodayTimesList = ({ today }) => {
   //console.log(props.map(x=> x.data.data.timings.Fajr))
   return (
     <>
       {today.map((obj, index) => (
         <ul className="TodayTimesList__ul-times-list" key={index}>
           <li className="TodayTimesList__ul-times-list__li">
-            <span>
-              {obj.data.date.gregorian.date} <span> i</span>
+            <span>{obj.data.date.gregorian.date} </span>
+            <span className="TodayTimesList__ul-times-list__li__span--information">
+              {" "}
+              i
             </span>{" "}
           </li>
           <li className="TodayTimesList__ul-times-list__li">
             <span>
               {obj.data.timings.Fajr} - {obj.data.timings.Sunrise}{" "}
-              <span> i</span>
+            </span>
+            <span className="TodayTimesList__ul-times-list__li__span--information">
+              {" "}
+              i
             </span>{" "}
           </li>
           <li className="TodayTimesList__ul-times-list__li">
-            <span>
-              {obj.data.timings.Dhuhr} <span> i</span>
+            <span>{obj.data.timings.Dhuhr} </span>{" "}
+            <span className="TodayTimesList__ul-times-list__li__span--information">
+              {" "}
+              i
             </span>{" "}
           </li>
           <li className="TodayTimesList__ul-times-list__li">
-            <span>
-              {obj.data.timings.Asr} <span> i</span>
+            <span>{obj.data.timings.Asr} </span>
+            <span className="TodayTimesList__ul-times-list__li__span--information">
+              {" "}
+              i
             </span>{" "}
           </li>
           <li className="TodayTimesList__ul-times-list__li">
-            <span>
-              {obj.data.timings.Maghrib} <span> i</span>
+            <span>{obj.data.timings.Maghrib} </span>{" "}
+            <span className="TodayTimesList__ul-times-list__li__span--information">
+              {" "}
+              i
             </span>{" "}
           </li>
           <li className="TodayTimesList__ul-times-list__li">
-            <span>
-              {obj.data.timings.Isha} <span> i</span>
+            <span>{obj.data.timings.Isha} </span>{" "}
+            <span className="TodayTimesList__ul-times-list__li__span--information">
+              {" "}
+              i
             </span>{" "}
           </li>
         </ul>
