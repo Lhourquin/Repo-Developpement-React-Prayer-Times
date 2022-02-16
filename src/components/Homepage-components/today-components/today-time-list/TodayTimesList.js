4a1638caa0bde1f72a5c4f3a64b5500955d21916
lhourquin <lhourquin@gmail.com>
2022-02-17 00:43:12 +0100

    styling hadith container

--- src/components/Homepage-components/today-components/today-time-list/TodayTimesList.js
+++ src/components/Homepage-components/today-components/today-time-list/TodayTimesList.js
@@ -7,35 +7,41 @@ export const TodayTimesList = ({ today }) => {
   const [listOfHadith, setListOfHadith] = useState([
     {
       fajrHadith: {
+        title: " Prier le Fajr avant le lever complet du soleil (shourouq)",
         hadith: `
-          Prier le Fajr avant le lever complet du soleil (shourouq)
+         
  
-      Au nom d'Allah, le Tout Miséricordieux, le Très Miséricordieux.
       Le Prophète (que la prière d'Allah et Son salut soient sur lui) a dit:
        « Le temps de la prière du matin (fajr) commence  dès la levée de l’aube et dure jusqu’au lever du soleil. 
        Lorsque le soleil pointe (c'est à dire au moment du shourouq), abstiens-toi de prier, car l’astre se lève entre les deux cornes de Satan»  
-       (rapporté par Mouslim, 612).`,
+`,
+        source: "(rapporté par Mouslim, 612).",
+        postScriptum: "",
       },
+
       dhohrHadith: {
+        title: "Le temps de la prière du dhohr",
         hadith: `
-        Le temps de la prière du dhohr
  
-      Au nom d'Allah, le Tout Miséricordieux, le Très Miséricordieux.
       Le Prophète (que la prière d'Allah et Son salut soient sur lui) a dit:
         Le temps du dhohr arrive lorsque le soleil dépasse le zénith jusqu’au moment
         où l’ombre de l’homme est égale à sa longueur jusqu’à la prière du ‘asr.`,
+        source: "",
+        postScriptum: "",
       },
       asrHadith: {
+        title: "Prier le Asr avant la fin de son temps",
         hadith: `
-        Prier le Asr avant la fin de son temps
-        Au nom d'Allah, le Tout Miséricordieux, le Très Miséricordieux.
 
         Abou El Malih a dit : 
         « Par un jour nuageux, nous étions en expédition avec Boraïda. – Hâtez l’heure de la prière de l’après-midi (‘asr) nous fit-il remarquer, 
         car le Prophète (que la prière d'Allah et Son salut soient sur lui)a dit : « Celui qui laisse passer le temps* de la prière de l’après-midi (le asr) verra ses actions annulées ». » 
-        [Sahih al-Bukhari 553]
+       
         
-        * Il se termine quand le soleil devient pâle (en général ≈ 15 minutes avant le maghrib) ; pourtant il est rapporté dans un hadith authentique que son temps se termine au moment du coucher du soleil : من أدرك ركعة من العصر قبل أن تغرب الشمس فقد أدرك العصر  → « Celui qui a atteint une rak’a de la prière du ‘asr avant que le soleil ne se couche a atteint le ‘asr » (Al Boukari).
+        
+        
+        `,
+        postScriptum: `* Il se termine quand le soleil devient pâle (en général ≈ 15 minutes avant le maghrib) ; pourtant il est rapporté dans un hadith authentique que son temps se termine au moment du coucher du soleil : من أدرك ركعة من العصر قبل أن تغرب الشمس فقد أدرك العصر  → « Celui qui a atteint une rak’a de la prière du ‘asr avant que le soleil ne se couche a atteint le ‘asr » (Al Boukari).
 
  
 
@@ -43,38 +49,40 @@ export const TodayTimesList = ({ today }) => {
         
         Et donc le ‘asr a 2 temps : un temps optionnel qui va jusqu’à la pâleur du soleil, et un temps de daroûrah (nécessité absolue) qui va jusqu’au coucher du soleil 
         L'imam Nawawi (mort en 676) dans Charh Sahih Mouslim 1 p 450 : 
-        « Sache que le fait de retarder la prière jusqu'au moment de la fraicheur est légiféré pour la prière du dohr et n'est pas légiféré pour la prière du 'asr pour l'ensemble des savants sauf Ach'hath Al Maliki; et cela n'est pas légiféré pour la prière du vendredi pour la majorité des savants ».
-        
-        `,
+        « Sache que le fait de retarder la prière jusqu'au moment de la fraicheur est légiféré pour la prière du dohr et n'est pas légiféré pour la prière du 'asr pour l'ensemble des savants sauf Ach'hath Al Maliki; et cela n'est pas légiféré pour la prière du vendredi pour la majorité des savants ».`,
+        source: " [Sahih al-Bukhari 553]",
       },
       maghrebHadith: {
-        hadith: `L'incitation à prier la prière du maghreb au début de son temps
+        title:
+          "L'incitation à prier la prière du maghreb au début de son temps",
+        hadith: `
  
-      Au nom d'Allah, le Tout Miséricordieux, le Très Miséricordieux.
       
       D'après Souweyd Ibn Ghafala, 'Omar Ibn Al Khattab (qu'Allah l'agrée) a dit : « Priez cette prière alors que les chemins sont lumineux » ; c'est à dire le maghreb. (*)
-      (Rapporté par Ibn Abi Chayba dans son Moussannaf n°3354 et authentifié par Cheikh Chathri dans sa correction du Moussannaf de Ibn Abi Chayba vol 3 p 220)
       
-      (*) Ainsi, le sens de ce texte est qu'il est recommandé de prier le maghreb au début de son temps.
+      `,
+        source:
+          "(Rapporté par Ibn Abi Chayba dans son Moussannaf n°3354 et authentifié par Cheikh Chathri dans sa correction du Moussannaf de Ibn Abi Chayba vol 3 p 220)",
+        postScriptum: `(*) Ainsi, le sens de ce texte est qu'il est recommandé de prier le maghreb au début de son temps.
       
-      L'imam Ibn Abi Chayba (mort en 235 du calendrier hégirien) a cité ce hadith dans son Moussannaf dans le chapitre intitulé : - Ceux qui étaient d'avis qu'il faut s'empresser de prier le maghreb -.
-      (Moussannaf de Ibn Abi Chayba vol 3 p 219)`,
+    L'imam Ibn Abi Chayba (mort en 235 du calendrier hégirien) a cité ce hadith dans son Moussannaf dans le chapitre intitulé : - Ceux qui étaient d'avis qu'il faut s'empresser de prier le maghreb -.
+    (Moussannaf de Ibn Abi Chayba vol 3 p 219)`,
       },
       ichaHadith: {
+        title: "        Prier le Icha avant la fin de son temps        ",
         hadith: `
         
-        Prier le Icha avant la fin de son temps
-        Au nom d'Allah, le Tout Miséricordieux, le Très Miséricordieux.
         
         D'après 'Abdallah Ibn 'Amr (qu'Allah les agrée), le Prophète (que la prière d'Allah et Son salut soient sur lui) a dit:
          « Le temps de la prière du 'icha est jusqu'au milieu de la nuit ».
-        (Rapporté par Mouslim dans son Sahih n°612)
         `,
+        source: `        (Rapporté par Mouslim dans son Sahih n°612)
+        `,
+        postScriptum: ``,
       },
       qiyammHadith: {
+        title: "        Le mérite de la prière de nuit        ",
         hadith: `
-        Le mérite de la prière de nuit
-        Au nom d'Allah, le Tout Miséricordieux, le Très Miséricordieux.
 
         le Prophète (que la prière d'Allah et Son salut soient sur lui) a dit:a dit : 
         « Chaque nuit, quand parvient le dernier tiers de la nuit, notre Seigneur, 
@@ -93,13 +101,15 @@ export const TodayTimesList = ({ today }) => {
         « Certes la meilleure prière après les prières obligatoires est la prière au coeur de la nuit et le meilleur jeûne après le ramadan est le mois d'Allah que vous appelez mouharam ».
         (Rapporté par Nasai et authentifié par Cheikh Albani dans Sahih Targhib n°1016)
 
-        Remarque :
-
-        Pourquoi dans ces textes le Prophète (que la prière d'Allah et Son salut soient sur lui) a t-il dit « le mois d'Allah » en parlant du mois de Mouharam alors qu'il y a d'autres mois qui sont équivalent à Mouhram dans le mérite ou qui sont meilleurs comme Ramadan ?
-
-        L'imam Souyouti (mort en 911) a dit : « La réponse est que ceci est un mois islamique à la différence des autres mois de l'année, dont les noms n'ont pas changés depuis la période pré-islamique.
-        En effet, le nom de Mouharam durant la période pré-islamique était -Safar Al Awal- et celui qui le suivait -Safar Al Thani-. Puis lorsque l'Islam est venu ce mois a été appelé Mouharam et c'est pour cela qu'il a été attribué à Allah ».
-        (Al Dibaj Charh Sahih Mouslim Ibn Hajjaj vol 3 p 251)`,
+       `,
+        source: "",
+        postScriptum: ` Remarque :
+ 
+       Pourquoi dans ces textes le Prophète (que la prière d'Allah et Son salut soient sur lui) a t-il dit « le mois d'Allah » en parlant du mois de Mouharam alors qu'il y a d'autres mois qui sont équivalent à Mouhram dans le mérite ou qui sont meilleurs comme Ramadan ?
+ 
+       L'imam Souyouti (mort en 911) a dit : « La réponse est que ceci est un mois islamique à la différence des autres mois de l'année, dont les noms n'ont pas changés depuis la période pré-islamique.
+       En effet, le nom de Mouharam durant la période pré-islamique était -Safar Al Awal- et celui qui le suivait -Safar Al Thani-. Puis lorsque l'Islam est venu ce mois a été appelé Mouharam et c'est pour cela qu'il a été attribué à Allah ».
+       (Al Dibaj Charh Sahih Mouslim Ibn Hajjaj vol 3 p 251)`,
       },
     },
   ]);
@@ -109,29 +119,59 @@ export const TodayTimesList = ({ today }) => {
   const toggleDisplay = (index) => {
     if (index == 1) {
       setDisplayHadith((boolean) => !boolean);
-      setCurrentHadith(listOfHadith[0].fajrHadith.hadith);
+      setCurrentHadith({
+        title: listOfHadith[0].fajrHadith.title,
+        hadith: listOfHadith[0].fajrHadith.hadith,
+        source: listOfHadith[0].fajrHadith.source,
+        postScriptum: listOfHadith[0].fajrHadith.postScriptum,
+      });
     } else if (index === 2) {
       setDisplayHadith((boolean) => !boolean);
-      setCurrentHadith(listOfHadith[0].dhohrHadith.hadith);
+      setCurrentHadith({
+        title: listOfHadith[0].dhohrHadith.title,
+        hadith: listOfHadith[0].dhohrHadith.hadith,
+        source: listOfHadith[0].dhohrHadith.source,
+        postScriptum: listOfHadith[0].dhohrHadith.postScriptum,
+      });
     } else if (index === 3) {
       setDisplayHadith((boolean) => !boolean);
-      setCurrentHadith(listOfHadith[0].asrHadith.hadith);
+      setCurrentHadith({
+        title: listOfHadith[0].asrHadith.title,
+        hadith: listOfHadith[0].asrHadith.hadith,
+        source: listOfHadith[0].asrHadith.source,
+        postScriptum: listOfHadith[0].asrHadith.postScriptum,
+      });
     } else if (index === 4) {
       setDisplayHadith((boolean) => !boolean);
-      setCurrentHadith(listOfHadith[0].maghrebHadith.hadith);
+      setCurrentHadith({
+        title: listOfHadith[0].maghrebHadith.title,
+        hadith: listOfHadith[0].maghrebHadith.hadith,
+        source: listOfHadith[0].maghrebHadith.source,
+        postScriptum: listOfHadith[0].maghrebHadith.postScriptum,
+      });
     } else if (index === 5) {
       setDisplayHadith((boolean) => !boolean);
-      setCurrentHadith(listOfHadith[0].ichaHadith.hadith);
+      setCurrentHadith({
+        title: listOfHadith[0].ichaHadith.title,
+        hadith: listOfHadith[0].ichaHadith.hadith,
+        source: listOfHadith[0].ichaHadith.source,
+        postScriptum: listOfHadith[0].ichaHadith.postScriptum,
+      });
     } else if (index === 6) {
       setDisplayHadith((boolean) => !boolean);
-      setCurrentHadith(listOfHadith[0].qiyammHadith.hadith);
+      setCurrentHadith({
+        title: listOfHadith[0].qiyammHadith.title,
+        hadith: listOfHadith[0].qiyammHadith.hadith,
+        source: listOfHadith[0].qiyammHadith.source,
+        postScriptum: listOfHadith[0].qiyammHadith.postScriptum,
+      });
     }
   };
 
   const closeHadith = () => {
     setDisplayHadith((boolean) => !boolean);
   };
-  // console.log(listOfHadith[0].maghrebHadith.hadith);
+  console.log(currentHadith);
   return (
     <>
       {displayHadith === true ? (
@@ -139,14 +179,26 @@ export const TodayTimesList = ({ today }) => {
           style={{
             background: "black",
             width: "100%",
-            height: "110%",
+            height: "100%",
+            top: "0",
             opacity: "0.8",
             position: "absolute",
           }}
         >
-          <div style={{ textAlign: "center" }}>
-            <button onClick={closeHadith}>Cross</button>
-            <div>{currentHadith}</div>
+          <button onClick={closeHadith}>Cross</button>
+
+          <div
+            className="hadith-container"
+          >
+            <div>
+              <h2>{currentHadith.title}</h2>
+              <h3>
+                Au nom d'Allah, le Tout Miséricordieux, le Très Miséricordieux.
+              </h3>
+              <p>{currentHadith.hadith}</p>
+              <p>{currentHadith.source}</p>
+              <p>{currentHadith.postScriptum}</p>
+            </div>
           </div>
         </div>
       ) : (
