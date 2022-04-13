5f9ca576b133932ba4cce40de90e0e47a1b42187
lhourquin <lhourquin@gmail.com>
2022-04-13 03:26:27 +0200

    change size of card and add text instruction

--- src/components/Homepage-components/calendar-components/Calendar.js
+++ src/components/Homepage-components/calendar-components/Calendar.js
@@ -104,6 +104,7 @@ export const Calendar = ({
 
   return (
     <>
+
       {displayTimesOfTheDateClicked ? (
         <div
           style={{
@@ -115,7 +116,23 @@ export const Calendar = ({
           }}
         >
           <div style={{ color: "black" }}>
-            <button className="closeTimesOfDates"
+           
+            <div
+              className={
+                now >
+                  new Date(
+                    `${currentTimesOfTheDateClicked.date.gregorian.month.en} ${currentTimesOfTheDateClicked.date.gregorian.day}, ${currentTimesOfTheDateClicked.date.gregorian.year} 00:00:00`
+                  )
+                  ? "containerCurrentTimesClicked-passed"
+                  : now ===
+                    new Date(
+                      `${currentTimesOfTheDateClicked.date.gregorian.month.en} ${currentTimesOfTheDateClicked.date.gregorian.day}, ${currentTimesOfTheDateClicked.date.gregorian.year} 00:00:00`
+                    ).setHours(0, 0, 0, 0)
+                    ? "containerCurrentTimesClicked-today"
+                    : "containerCurrentTimesClicked-normal"
+              }
+            >
+               <button className="closeTimesOfDates"
               onClick={closeTimesOfTheDate}
               style={
                 now >
@@ -132,21 +149,6 @@ export const Calendar = ({
               }>
               <i className="fas fa-times-circle"></i>
             </button>
-            <div
-              className={
-                now >
-                  new Date(
-                    `${currentTimesOfTheDateClicked.date.gregorian.month.en} ${currentTimesOfTheDateClicked.date.gregorian.day}, ${currentTimesOfTheDateClicked.date.gregorian.year} 00:00:00`
-                  )
-                  ? "containerCurrentTimesClicked-passed"
-                  : now ===
-                    new Date(
-                      `${currentTimesOfTheDateClicked.date.gregorian.month.en} ${currentTimesOfTheDateClicked.date.gregorian.day}, ${currentTimesOfTheDateClicked.date.gregorian.year} 00:00:00`
-                    ).setHours(0, 0, 0, 0)
-                    ? "containerCurrentTimesClicked-today"
-                    : "containerCurrentTimesClicked-normal"
-              }
-            >
               <ul className="ClickedTimesList__ul-times-list">
                 <li className="ClickedTimesList__ul-times-list__li-date">
                   {currentTimesOfTheDateClicked.date.gregorian.date}
@@ -284,77 +286,76 @@ export const Calendar = ({
       ) : (
         ""
       )}
-      <div
-        style={
-          isCalendar ? { opacity: "1", height: 0 } : {  opacity: "0", display: "none" }
-        }
-        className="calendar">
-        {
-          calendar.length === 0 ? "" : 
-          <>
-          <div className="calendar-container-month-degreeSeclect">
-            <div>
-              <h2 className="calendar-title-month">
-                {acutalMonth.charAt(0).toUpperCase() + acutalMonth.slice(1)}{" "}
-                <span
-                  onClick={displayInfosMonth}
-                  className="calendar-span-infos-month"
-                >
-                  {" "}
-                  <i className="fas fa-info-circle info-icons-calendar"></i>
+      {
+        calendar.length === 0 ? "" :
+          <div
+            style={
+              isCalendar ? { opacity: "1", height: 0 } : { opacity: "0", display: "none" }
+            }
+            className="calendar">
+
+            <div className="calendar-container-month-degreeSeclect">
+              <div>
+                <h2 className="calendar-title-month">
+                  {acutalMonth.charAt(0).toUpperCase() + acutalMonth.slice(1)}{" "}
+                  <span
+                    onClick={displayInfosMonth}
+                    className="calendar-span-infos-month"
+                  >
+                    {" "}
+                    <i className="fas fa-info-circle info-icons-calendar"></i>
+                  </span>
+                </h2>
+              </div>
+
+              <div className="Calendar__div-container-span-select">
+                <span style={{ fontSize: "10px", color: "#aa75ff" }}>
+                  <i className="fas fa-map-marker-alt localisation-marker-pannel"></i>{" "}
+
+                  {city.charAt(0).toUpperCase() + city.slice(1).toLowerCase()},{" "}
+                  {country.charAt(0).toUpperCase() + country.slice(1).toLowerCase()}
+                  <br></br>
+                  Angle :{" "}
+                  {selectedMethodStringValue === "" ||
+                    selectedMethodStringValue === "Sélectionner un angle"
+                    ? "Amérique du Nord (15°)"
+                    : selectedMethodStringValue}
                 </span>
-              </h2>
+
+                <select onChange={getAngleOptionValue}>
+                  {method.map((currentTimesOfTheDateClicked, index) => (
+                    <option key={index} value={currentTimesOfTheDateClicked.value}>
+                      {currentTimesOfTheDateClicked.stringValue}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
+            <table>
+              <thead className="thead-name-of-times">
+                <tr className="name-of-times-list">
+                  <td>Date</td>
+                  <td>Date hégirienne</td>
+                  <td>Fajr</td>
+                  <td>Shourouq</td>
+                  <td>Dhor</td>
+                  <td>Asr</td>
+                  <td>Maghreb</td>
+                  <td>Icha</td>
+                  <td>Mi-nuit</td>
+                </tr>
+              </thead>
+              <RowList
+                calendar={calendar}
+                displayTimesOfTheDateClicked={displayTimesOfTheDateClicked}
+                displayTimesOfTheDate={displayTimesOfTheDate}
+                mediaChangeInMobileOrTablet={mediaChangeInMobileOrTablet}
+                mediaSize={mediaSize}
+              />
+            </table>
 
-            <div className="Calendar__div-container-span-select">
-              <span style={{ fontSize: "10px", color: "#aa75ff" }}>
-                <i className="fas fa-map-marker-alt localisation-marker-pannel"></i>{" "}
-
-                {city.charAt(0).toUpperCase() + city.slice(1).toLowerCase()},{" "}
-                {country.charAt(0).toUpperCase() + country.slice(1).toLowerCase()}
-                <br></br>
-                Angle :{" "}
-                {selectedMethodStringValue === "" ||
-                  selectedMethodStringValue === "Sélectionner un angle"
-                  ? "Amérique du Nord (15°)"
-                  : selectedMethodStringValue}
-              </span>
-
-              <select onChange={getAngleOptionValue}>
-                {method.map((currentTimesOfTheDateClicked, index) => (
-                  <option key={index} value={currentTimesOfTheDateClicked.value}>
-                    {currentTimesOfTheDateClicked.stringValue}
-                  </option>
-                ))}
-              </select>
-            </div>
           </div>
-          <table>
-          <thead className="thead-name-of-times">
-            <tr className="name-of-times-list">
-              <td>Date</td>
-              <td>Date hégirienne</td>
-              <td>Fajr</td>
-              <td>Shourouq</td>
-              <td>Dhor</td>
-              <td>Asr</td>
-              <td>Maghreb</td>
-              <td>Icha</td>
-              <td>Mi-nuit</td>
-            </tr>
-          </thead>
-          <RowList
-            calendar={calendar}
-            displayTimesOfTheDateClicked={displayTimesOfTheDateClicked}
-            displayTimesOfTheDate={displayTimesOfTheDate}
-            mediaChangeInMobileOrTablet={mediaChangeInMobileOrTablet}
-            mediaSize={mediaSize}
-          />
-        </table>
-        </>
-        }
-        
-      </div>
+      }
     </>
   );
 };
